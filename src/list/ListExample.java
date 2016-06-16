14ba98e128fdb1ccef94551d8276da223141fdd9
mobarakice <mobarak.ice.06@gmail.coom>
2016-06-16 20:19:43 +0600

    "Insert and get value"

--- src/list/ListExample.java
+++ src/list/ListExample.java
@@ -1,8 +1,36 @@
 package list;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ListExample {
+	
+	private List<String> mList;
 	public static void main(String[] args){
 		System.out.println("List example");
+		ListExample  example = new ListExample();
+		example.getList();
+		
+	}
+	
+	private List setList(List<String> list){
+	 list = new ArrayList<>();
+		for(int i=0;i<10;i++){
+			list.add("Index "+i);
+		}
+		return list;
+	}
+	
+	private void getList(){
+		List<String> list = new ArrayList<>();
+		list = setList(mList);
+		if(list.isEmpty()){
+			return;
+		}
+		for(String index:list){
+			System.out.println(index);
+		}
+		
 	}
 	
 
