b60975ac556b9e0ea9d61eff46f082ac4a56044a
mobarakice <mobarak.ice.06@gmail.coom>
2016-06-17 23:25:26 +0600

    "Removed element from list"

--- src/list/ListExample.java
+++ src/list/ListExample.java
@@ -10,10 +10,12 @@ public class ListExample {
 	private List<String> mList;
 	public static void main(String[] args){
 		ListExample  example = new ListExample();
-		example.getListForLoop();
-		example.getListForEachLoop();
-		example.getListIterator();
-		example.getListWhileLoop();
+//		example.getListForLoop();
+//		example.getListForEachLoop();
+//		example.getListIterator();
+//		example.getListWhileLoop();
+//		example.removeItems();
+		example.removeAllIetms();
 		
 	}
 	
@@ -63,7 +65,7 @@ public class ListExample {
 		}
 		System.out.println(" No \t Index ");
 		System.out.println("----\t ------");
-		Iterator<String> iterator = list.iterator();
+		Iterator iterator = list.iterator();
 		while(iterator.hasNext()){
 			System.out.println(iterator.next());
 		}
@@ -101,6 +103,57 @@ public class ListExample {
 //			System.out.println(iterator.next());
 //		}
 		
+	}
+	private void removeItems(){
+		List items = setList(mList);
+		if(items.isEmpty()){
+			return;
+		}
+		System.out.println("\n-----Before removing element-----");
+		System.out.println(items.toString());
+		System.out.println("\n-----After removing element-----");
+
+		// Remove a specific element 
+		
+//		if(items.get(1).equals("Index 1")){
+//			items.remove(1);
+//			System.out.println(items.toString());
+//		}else{
+//			System.out.println("Item is not exist.");
+//		}
+		
+		//remove all element
+//		if(!items.isEmpty()){
+//			items.removeAll(items);
+//		}
+		items.clear();
+		System.out.println(items.toString());
+		
+	}
+	private void removeAllIetms(){
+		// for loop example---
+		List<String>items = setList(mList);
+		if(items.isEmpty()){
+			return;
+		}
+		System.out.println("\n-----Before removing element-----");
+		System.out.println(items.toString());
+		System.out.println("\n-----After removing element-----");
+//		for(int i = (items.size()-1);i>=0;i--){
+//			items.remove(i);
+//			System.out.println(items.toString());
+//		}
+//		for(String item:new ArrayList<>(items)){
+//			items.remove(item);
+//			System.out.println(items.toString());
+//		}
+		Iterator<String> iterator = items.iterator();
+		while(iterator.hasNext()){
+			String s = iterator.next();
+			iterator.remove();
+			System.out.println("Removed: ");
+		}
+		System.out.println(items.toString());
 	}
 	
 
