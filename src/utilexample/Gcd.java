2af9e17a697565c4e6978af4e5fe3a98ae74154a
Mobarak Hosen <mobarak.ice.06@gmail.com>
2017-09-30 23:50:23 +0600

    Added GCD methods

--- src/utilexample/Gcd.java
+++ src/utilexample/Gcd.java
@@ -17,8 +17,38 @@ public class Gcd {
 		}
 	}
 	public static void main(String[] args) {
-		System.out.println(first(5,4));
+		System.out.println(gcdRecursiveMethod(42,56));
+		System.out.println(gcdDivisibleMethod(42,56));
+		System.out.println(gcdSubtractMethod(42,56));
 
 	}
+	
+	// Greatest common divisor
+	public static int gcdRecursiveMethod(int a, int b){
+		if(b==0){
+			return a;
+		}else{
+			return gcdRecursiveMethod(b,a%b);
+		}
+	}
+	public static int gcdDivisibleMethod(int a, int b){
+		while(b>0){
+			int temp = b;
+			b = a%b;
+			a= temp;
+		}
+		return a;
+	}
+	public static int gcdSubtractMethod(int a, int b){
+		while(a != b){
+			if(a>b){
+				a = a-b;
+			}else{
+				b = b-a;
+			}
+		}
+		return a;
+	}
+
 
 }
